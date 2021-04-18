1238000f81ddd9edaa5d94ea23c0727c66b51cd9
DiegoMutre <tysonmutre10@gmail.com>
2021-04-18 17:18:41 -0500

    Create About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,7 @@
+export const About = () => {
+    return (
+        <section className="section-2" id="section-2">
+            <h1 className="section-heading section-2-heading">About Me</h1>
+        </section>
+    );
+};
