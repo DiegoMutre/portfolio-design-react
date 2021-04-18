7824219e635ff5a8b5032990a82c527885e0f703
DiegoMutre <tysonmutre10@gmail.com>
2021-04-18 15:39:39 -0500

    Create Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+export const Hero = () => (
+    <section className="section-1 center" id="section-1">
+        <h1 className="section-1-heading">Web Developer</h1>
+        {/* Image here */}
+        <img src="" alt="John Smith" />
+    </section>
+);
