844b9436f45e53a1644149c3f393eb4de8287b6b
DiegoMutre <tysonmutre10@gmail.com>
2021-04-18 17:31:14 -0500

    Create About styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,9 @@
+import "./About.css";
+
 export const About = () => {
     return (
         <section className="section-2" id="section-2">
-            <h1 className="section-heading section-2-heading">About Me</h1>
+            <h1 className="section-heading">About Me</h1>
         </section>
     );
 };
