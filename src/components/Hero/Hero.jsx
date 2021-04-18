aa365482a4de7dd649380373c472dcb8040dabb5
DiegoMutre <tysonmutre10@gmail.com>
2021-04-18 15:47:26 -0500

    Finish Hero component structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,12 @@
+import ProfileImage from "../../images/john-smith.jpg";
+
 export const Hero = () => (
     <section className="section-1 center" id="section-1">
         <h1 className="section-1-heading">Web Developer</h1>
-        {/* Image here */}
-        <img src="" alt="John Smith" />
+        <img src={ProfileImage} alt="John Smith" className="person-img" />
+        <h3 className="person-name">John Smith</h3>
+        <a href="section-3" className="section-1-btn">
+            Projects
+        </a>
     </section>
 );
