8f59eb8d30355771c3836ae9e6cb539d6cb05027
DiegoMutre <tysonmutre10@gmail.com>
2021-04-18 18:17:20 -0500

    Use Services component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,13 @@
 import "./About.css";
 import { ProgressBarsWrapper } from "./ProgressBarsWrapper";
+import { Services } from "./Services";
 
 export const About = () => {
     return (
         <section className="section-2" id="section-2">
             <h1 className="section-heading">About Me</h1>
             <ProgressBarsWrapper />
+            <Services />
         </section>
     );
 };
