ff4c43a5d49595ee25d08848a82a83a13adb0df1
DiegoMutre <tysonmutre10@gmail.com>
2021-04-19 18:35:46 -0500

    Create Contact component

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+export const Contact = () => {
+    return (
+        <section className="section-4" id="section-4">
+            <h1 className="section-heading">Contact Me</h1>
+            <form className="contact-form center">
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <textarea placeholder="Message"></textarea>
+                <input
+                    type="submit"
+                    value="Submit"
+                    className="contact-form-btn"
+                />
+            </form>
+        </section>
+    );
+};
