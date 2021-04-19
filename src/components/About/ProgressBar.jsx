8aa2d4ec67379596c7bc00645f2d0ede36e93e8b
DiegoMutre <tysonmutre10@gmail.com>
2021-04-18 19:58:46 -0500

    Create observable for ProgressBar

--- src/components/About/ProgressBar.jsx
+++ src/components/About/ProgressBar.jsx
@@ -1,13 +1,29 @@
 import PropTypes from "prop-types";
+import { useEffect, useRef } from "react";
 
 export const ProgressBar = ({ skill }) => {
+    const progressRef = useRef();
+
+    useEffect(() => {
+        const progressBar = progressRef.current;
+        const observer = new IntersectionObserver(entries => {
+            // Check if is watching the element and if it doesn't already has a width
+            if (entries[0].isIntersecting && !entries[0].target.style.width) {
+                entries[0].target.style.width = `${skill.percent}%`;
+            }
+        });
+        observer.observe(progressBar);
+
+        return () => observer.unobserve(progressBar);
+    }, [skill]);
+
     return (
         <div className="progress-bar">
             <p className="progress-text">
                 {skill.technology}
                 <span>{skill.percent}</span>%
             </p>
-            <div className="progress-percent"></div>
+            <div className="progress-percent" ref={progressRef}></div>
         </div>
     );
 };
