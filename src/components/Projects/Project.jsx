9bb62e25d0f0d2c483d97ee9a9cb1bbd612dc374
DiegoMutre <tysonmutre10@gmail.com>
2021-04-19 07:30:06 -0500

    Create Project component

---
+++ src/components/Projects/Project.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+export const Project = ({ project }) => {
+    return (
+        <div className="project">
+            <div className="project-text">
+                <h2 className="project-name">{project.projectName}</h2>
+                <h4 className="project-technologies">{project.technologies}</h4>
+                <img
+                    src={project.imageURL}
+                    alt={project.projectName}
+                    className="project-img"
+                />
+                <a href={project.videoURL} className="project-link">
+                    Go to Video
+                </a>
+            </div>
+        </div>
+    );
+};
+
+Project.propTypes = {
+    project: PropTypes.object.isRequired,
+};
