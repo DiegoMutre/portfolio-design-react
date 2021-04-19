8801ca7e18d1dc99044248ec057d5101a5185be0
DiegoMutre <tysonmutre10@gmail.com>
2021-04-19 07:30:27 -0500

    Fetch Projects from API and show in Project component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { Project } from "./Project";
+
 export const Projects = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const data = await fetch("http://192.168.100.19:4000/projects");
+            const { projects } = await data.json();
+            setProjects(projects);
+        };
+
+        fetchProjects();
+    }, []);
+
     return (
         <section className="section-3" id="section-3">
             <h1 className="section-heading">My Projects</h1>
             <div className="projects-wrapper center">
-                {/* Each project here */}
+                {projects.map(project => (
+                    <Project key={project.id} project={project} />
+                ))}
             </div>
         </section>
     );
