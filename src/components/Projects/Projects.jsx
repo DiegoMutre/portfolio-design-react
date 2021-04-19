49eaa0b7b72b9a512300e2918cb04d2af6ff2249
DiegoMutre <tysonmutre10@gmail.com>
2021-04-19 06:44:46 -0500

    Create Projects component

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,10 @@
+export const Projects = () => {
+    return (
+        <section className="section-3" id="section-3">
+            <h1 className="section-heading">My Projects</h1>
+            <div className="projects-wrapper center">
+                {/* Each project here */}
+            </div>
+        </section>
+    );
+};
