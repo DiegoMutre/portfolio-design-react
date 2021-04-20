07073bc19e7025ab9d0db9058c886c4153d217dd
DiegoMutre <tysonmutre10@gmail.com>
2021-04-19 19:00:04 -0500

    Create Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+export const Footer = () => {
+    return (
+        <footer className="section-5 center">
+            <div className="social-media">
+                <a href="#none" className="social-media-link">
+                    <i className="fab fa-github-square"></i>
+                </a>
+                <a href="#none" className="social-media-link">
+                    <i className="fab fa-youtube"></i>
+                </a>
+                <a href="#none" className="social-media-link">
+                    <i className="fab fa-facebook-square"></i>
+                </a>
+                <a href="#none" className="social-media-link">
+                    <i className="fab fa-instagram-square"></i>
+                </a>
+            </div>
+            <p className="copyright">
+                Copyright &copy; TSY. All Rights Reserved
+            </p>
+        </footer>
+    );
+};
