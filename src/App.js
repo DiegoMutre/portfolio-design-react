3e7e51014d6df960ce4792c1cc31ca57c41b5bd8
DiegoMutre <tysonmutre10@gmail.com>
2021-04-19 19:00:32 -0500

    Use Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 import { Hero } from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 import { Projects } from "./components/Projects";
@@ -12,6 +13,7 @@ function App() {
             <About />
             <Projects />
             <Contact />
+            <Footer />
         </div>
     );
 }
